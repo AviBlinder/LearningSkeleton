523d9fa0dc13da499f7f5d10a592138a20882e8d
Avi Blinder <aviblinder@gmail.com>
2016-12-13 23:25:19 +0200

    formatting the skeleton

--- UI.r
+++ UI.r
@@ -1,42 +1,48 @@
-
+library(shinythemes)
 #A navlistPanel is like a tabPanel, except that the navigation links are on the left
 # instead of the top.
-
+?shinytheme
 fluidPage(
   
   
+  theme = shinytheme("yeti"),
   
   mainPanel(
-    tabsetPanel(
-      tabPanel("Plot", plotOutput("plot")),
-      tabPanel("Summary", verbatimTextOutput("summary")),
-      tabPanel("Table", tableOutput("table")),
-      navlistPanel(
-        "Internal navlist Panel",
-        tabPanel("Internal One",
-                 h3("This is the second panel")
-        ),
-        tabPanel("Internal Two",
-                 h3("This is the third panel")
-                 
-      )
-    )
-  ),
-  
-    
-  titlePanel("Navlist panel example"),
+
+  titlePanel("Learning Panel"),
   
   navlistPanel(
-    "Header",
-    tabPanel("First",
-             h3("This is the first panel")
+    "Clustering",
+    tabPanel(strong("Introduction"),
+             br(),
+             tags$div(
+               HTML(paste("This panel displays a plot only if the ", 
+                          tags$span(style="color:red", "Execute Forecast Models"), 
+                          " checkbox is marked and you press the ",
+                          tags$span(style="color:red", "Time Series Plots"),
+                          " button !",
+                          sep = ""))
+             ),
+             br(),
+             p("Please be patient, more that 16 models are being executed in the background"),
+             plotOutput("ggplot_forecast"),
+             br()
     ),
-    tabPanel("Second",
-             h3("This is the second panel")
+    tabPanel("The Tool",
+             h3("Here you'll learn about the tool")
     ),
-    tabPanel("Third",
-             h3("This is the third panel")
+    tabPanel("When to Use It",
+             h3("Here you'll learn about the tool")
+    ),
+    tabPanel("When Not to Use it",
+             h3("Here you'll learn about the tool")
+    ),
+    tabPanel("Hands-On",
+             h3("Here you'll learn about the tool")
     )
+    
+    
+    
   )
   )
 )
